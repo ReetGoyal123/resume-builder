4d1b0b64c5488ed4c94ad63bf6626d75bc9d3943
ReetGoyal123 <reetgoyal2006@gmail.com>
2025-08-20 09:32:02 +0530

    commit

--- src/templates/modern/atoms/SectionList.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from 'react';
-
-export const SectionList = ({ children }: { children: ReactNode | string }) => {
-  return <div className="text-xs mt-0.5">{children}</div>;
-};
